9a8abbc93e6171ad239d42512978626f513e1edc
danmxli <danmuxingli@gmail.com>
2024-01-02 16:25:35 -0500

    all_hashtags endpoint

--- api/metauser/urls.py
+++ api/metauser/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.root, name="root")
+    path("", views.root, name="root"),
+    path("all_hashtags/<str:username>", views.all_hashtags, name="all_hashtags")
 ]
--- api/metauser/views.py
+++ api/metauser/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse, JsonResponse
 
 def root(request):
     return JsonResponse({"message": "root route reached"})
+
+def all_hashtags(request, username):
+    """TODO"""
+    return JsonResponse({"message": username})
