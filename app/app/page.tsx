1208e2fd494a37203ee0536298006c3625a3dbb9
danmxli <danmuxingli@gmail.com>
2024-01-02 15:22:09 -0500

    remove template utils

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,39 +1,23 @@
 import AuthButton from '../components/AuthButton'
-import { createClient } from '@/utils/supabase/server'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
-import { cookies } from 'next/headers'
 
 export default async function Index() {
-  const cookieStore = cookies()
-
-  const canInitSupabaseClient = () => {
-    // This function is just for the interactive tutorial.
-    // Feel free to remove it once you have Supabase connected.
-    try {
-      createClient(cookieStore)
-      return true
-    } catch (e) {
-      return false
-    }
-  }
-
-  const isSupabaseConnected = canInitSupabaseClient()
 
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
       <nav className="w-full flex justify-center border-b h-16">
         <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
-          {isSupabaseConnected && <AuthButton />}
+          <AuthButton />
         </div>
       </nav>
 
       <div className="animate-in flex-1 flex flex-col gap-20">
         <Header />
-        <main className="flex-1 flex flex-col">          
+        <main className="flex-1 flex flex-col">
         </main>
       </div>
-      
+
       <Footer />
     </div>
   )
