a6de6e1b0199b9d8ee9fe540b6d50f88db17260e
danmxli <danmuxingli@gmail.com>
2024-01-06 12:57:21 -0500

    rest_framework in installed apps, make migration

--- api/metauser/urls.py
+++ api/metauser/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.root, name="root"),
-    path("all_hashtags/<str:username>", views.all_hashtags, name="all_hashtags")
+    path("", views.root, name="root")
 ]
--- api/metauser/views.py
+++ api/metauser/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 
+@api_view(['GET'])
 def root(request):
-    return JsonResponse({"message": "root route reached"})
-
-def all_hashtags(request, username):
-    """TODO"""
-    return JsonResponse({"message": username})
+    return Response()
