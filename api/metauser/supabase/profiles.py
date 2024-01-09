949f84d76c8a08b9357bdd461874820bbe0476a5
danmxli <danmuxingli@gmail.com>
2024-01-09 17:50:31 -0500

    supabase testing method

--- api/metauser/supabase/profiles.py
+++ api/metauser/supabase/profiles.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 from decouple import config
 from supabase import create_client, Client
+from typing import List, Dict
 
 load_dotenv()
 url: str = config('SUPABASE_URL')
@@ -13,4 +14,7 @@ class Profiles:
         self.supabase: Client = create_client(url, key)
         pass
 
-    
+    def select_all(self) -> List:
+        # testing connectivity purposes only
+        response = self.supabase.table('profiles').select("*").execute()
+        return response
--- api/metauser/views.py
+++ api/metauser/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
 # serializers for request body validation
-from metauser.serializer import HashtagDataSerializer, UserDataSerializer
+from metauser.serializer import HashtagDataSerializer
 
 # TODO helper classes
 # from metauser.entity_recognition.image_recognizer import ImageRecognizer
